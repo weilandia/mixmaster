1bdc2be98c5e4bc03d62244fac903bea93bd5811
Nick Weiland <nickweiland@gmail.com>
2016-02-21 12:31:33 -0700

    Add artists implementation

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,23 @@
+class ArtistsController < ApplicationController
+  def index
+  end
+  
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create(artist_params)
+    redirect_to @artist
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+
+private
+  def artist_params
+    params.require(:artist).permit(:name, :image_path)
+  end
+end
